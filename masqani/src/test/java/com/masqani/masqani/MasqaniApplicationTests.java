d63989b037260f51f63f797248e0f7bba6a4da26
Flavian Maina <FLAVIAN.WAMBETI@equitybank.co.ke>
2024-09-09 15:40:33 +0300

    api setup

---
+++ masqani/src/test/java/com/masqani/masqani/MasqaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masqani.masqani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasqaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
