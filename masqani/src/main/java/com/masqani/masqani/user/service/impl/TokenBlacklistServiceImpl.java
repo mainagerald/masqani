278d6a171689d87f869c8f566d0f0f3438785871
Flavian Maina <FLAVIAN.WAMBETI@equitybank.co.ke>
2024-12-31 11:03:22 +0300

    hazel to caffeine. reduce complexities.

--- masqani/src/main/java/com/masqani/masqani/user/service/impl/TokenBlacklistServiceImpl.java
+++ masqani/src/main/java/com/masqani/masqani/user/service/impl/TokenBlacklistServiceImpl.java
@@ -1,44 +1,56 @@
 package com.masqani.masqani.user.service.impl;
 
-
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.map.IMap;
 import com.masqani.masqani.user.service.JwtService;
 import com.masqani.masqani.user.service.TokenBlacklistService;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class TokenBlacklistServiceImpl implements TokenBlacklistService {
-    private final HazelcastInstance hazelcastInstance;
+    private final CacheManager cacheManager;
     private final JwtService jwtService;
 
+    private static final String TOKEN_BLACKLIST_CACHE = "tokenBlacklistCache";
+
     public void blacklistToken(String token) {
-        IMap<String, Boolean> tokenBlacklist =
-                hazelcastInstance.getMap("tokenBlacklistCache");
+        Cache cache = cacheManager.getCache(TOKEN_BLACKLIST_CACHE);
+        if (cache == null) {
+            log.error("Cache '{}' not found", TOKEN_BLACKLIST_CACHE);
+            return;
+        }
+
         try {
             Claims claims = jwtService.extractAllClaims(token);
             long expirationTime = claims.getExpiration().getTime();
             long currentTime = System.currentTimeMillis();
-            long timeToLive = Math.max(0, expirationTime - currentTime);
 
-            tokenBlacklist.put(token, true, timeToLive, TimeUnit.MILLISECONDS);
-            log.info("Token blacklisted successfully");
+            long timeToLive = Math.max(0, expirationTime - currentTime);
+            if (timeToLive > 0) {
+                cache.put(token, true);
+                log.info("Token blacklisted successfully for {} ms", timeToLive);
+            } else {
+                log.warn("Token has already expired and cannot be blacklisted");
+            }
         } catch (Exception e) {
             log.error("Error blacklisting token", e);
         }
     }
+
     public boolean isTokenBlacklisted(String token) {
-        log.info("checking if token {} is blacklisted", token);
-        ConcurrentMap<String, Boolean> tokenBlacklist =
-                hazelcastInstance.getMap("tokenBlacklistCache");
-        return tokenBlacklist.containsKey(token);
+        log.info("Checking if token {} is blacklisted", token);
+        Cache cache = cacheManager.getCache(TOKEN_BLACKLIST_CACHE);
+        if (cache == null) {
+            log.error("Cache '{}' does not exist", TOKEN_BLACKLIST_CACHE);
+            return false;
+        }
+
+        Boolean isBlacklisted = cache.get(token, Boolean.class);
+        return Boolean.TRUE.equals(isBlacklisted);
     }
 }
