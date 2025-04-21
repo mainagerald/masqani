5fcf35712c2679bac2ae0ab0588c7852cfa7a6a4
unknown <flavianmaina2401@gmail.com>
2025-04-21 17:42:46 +0300

    mem bank

--- client/src/app/components/landlord/model/listing-picture.model.ts
+++
@@ -1,4 +0,0 @@
-export interface NewListingPictures{
-    file: File,
-    picUrl: string
-}
--- client/src/app/core/model/user.model.ts
+++
@@ -1,7 +0,0 @@
-export interface UserModel {
-    firstName?: string,
-    lastName?: string,
-    email?: string,
-    imageUrl?: string,
-    authorities?: string[];
-}
